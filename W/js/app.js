8534fccd92b9346d74259d20c3bf61e33181119b
Zach <zacbenincasa@gamil.com>
2022-07-23 10:12:34 -0600

    added simple variable to app.js working hard to rmemeber cookies, added ids to most everything so I can single it out in the future.

--- W/js/app.js
+++ W/js/app.js
@@ -1,4 +1,26 @@
-Cookies.set(document.getElementById(torchic))
-cookies.set(document.getElementById(todadile))
-Cookies.set(document.getElementById(bulbasaur))
-Cookies.set(document.getElementById(miltank))
+let first={
+    name:"torchic",
+    health:25,
+    attacks:["scratch","tackle","ember"],
+    image:document.getElementById("torchic")
+
+}
+let second ={
+    name:"Todadile",
+    health:25,
+    attacks:["scratch", "water gun", "tackle"],
+    image: document.getElementById("todadile")
+}
+let third= {
+    name: "bulbasaur",
+    health:25,
+    attacks:[ "vine whip", "tackle", "scratch"],
+    image:document.getElementById("bulbasaur")
+}
+let fourth={
+    name:"miltank",
+    health:25,
+    attacks:["rollout", "tackle", "scratch"],
+    image:document.getElementById("miltank")
+}
+
