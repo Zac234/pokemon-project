9a767df73f337153a42e2cbdd82c39489826124a
Zach <zacbenincasa@gamil.com>
2022-07-23 09:45:11 -0600

    added sprites to pokemon as well as hopefully the correct cookie setup

---
+++ W/js/app.js
@@ -0,0 +1,4 @@
+Cookies.set(document.getElementById(torchic))
+cookies.set(document.getElementById(todadile))
+Cookies.set(document.getElementById(bulbasaur))
+Cookies.set(document.getElementById(miltank))
