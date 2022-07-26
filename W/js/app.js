550d64c9d4be09afb9d6e733d7e6992f48f4c587
Zach <zacbenincasa@gamil.com>
2022-07-25 20:42:45 -0600

    added correct cookie formula

--- W/js/app.js
+++ W/js/app.js
@@ -3,9 +3,39 @@ function pick_torchic(details){
     cookies.set(user,first_json)
     cookies.set(user_health, first["health"])
 
-    let computer_json=Json.stringify(fourth)
+    let computer_json=JSON.stringify(fourth)
     cookies.set(computer_selection, fourth_json)
     cookies.set(computer_health,fourth["health"])
+}
+function pick_todadile(details){
+    let second_json=JSON.stringify(second)
+    cookies.set(user, second_json)
+    cookies.set(user_health, second[health])
+
+    let computer_json=JSON.stringify(fourth)
+    cookies.set(computer_selection,forth_json)
+    cookies.set(computer_health, fourth["health"])
+
+}
+
+function pick_bulbasaur(details){
+    let third_json = JSON.stringify(third)
+    cookies.set(user, third_json)
+    cookies.set(user_health, third["health"])
+
+    let computer_json=JSON.stringify(fourth)
+    cookies.set(computer_selection,forth_json)
+    cookies.set(computer_health, fourth["health"])
+}
+
+function pick_miltank(details){
+    let fourth_json=JSON.stringify(fourth)
+    cookies.set(user, fourth_json)
+    cookies.set(user_health, fourth["health"])
+
+    let computer_json=JSON.stringify(fourth)
+    cookies.set(computer_selection,forth_json)
+    cookies.set(computer_health, fourth["health"])
 }
 
 
@@ -45,4 +75,3 @@ cookies.set(user,fourth_json)
 
 
 
-let cookies
